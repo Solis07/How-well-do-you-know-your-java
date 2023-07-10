acd0e446d9b795f01097406be00cd4d6d9be8947
L.J Solis <l.jsolis@L.lan>
2023-07-09 20:56:51 -0500

    Added the questions to my javascript and began to work on the structure in html.

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+var questions = [
+  {
+     title: 'Commonly used data types DO NOT include:',
+     choices:['Strings', 'Booleans', 'Alerts', 'Numbers'],
+     answer: 'Alerts',
+  },
+  {
+     title: 'The condition in an if / else statement is enclosed within _____.',
+     choices:['Quotes', 'Curly brackets', 'Parentheses', 'Square brackets'],
+     answer: 'Parentheses',
+  },
+
+  {
+     title: 'Arrays in Javascript can be used to store _____.',
+     choices:['Numbers of strings', 'Other arrays', 'Booleans', 'All of the above'],
+     answer: 'All of the above',
+  },
+  {
+     title: 'String values must be enclosed within ____ when being assigned to variables.',
+     choices:['Commas', 'Curly brackets', 'Quotes', 'Parentheses'],
+     answer: 'Quotes',
+  },
+  {
+     title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+     choices:['JavaScript', 'Terminal/Bash', 'For loops', 'Console.log'],
+     answer:'Console.log',
+  },
+]
